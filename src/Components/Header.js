81b5b4a312386e9d66ef0a68b866933eda96ecaf
Hijazimohammed <30778440+Hijazimohammed@users.noreply.github.com>
2022-09-03 15:37:11 +0300

    header-commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div className='row tit'>
-      <h4 className=''>wellcom to Momen Expense Manager </h4>
+      <h4 className=''>wellcom to Expense Manager </h4>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
